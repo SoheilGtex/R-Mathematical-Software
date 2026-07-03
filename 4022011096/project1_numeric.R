a1fffdede147e5357878ec8133e03cec9a59188c
mahya yarmohammadi <mahyayarmohamadi@gmail.com>
2026-07-03 14:53:09 +0330

    Create project1_numeric.R

---
+++ 4022011096/project1_numeric.R
@@ -0,0 +1,5 @@
+mkdir submissions/4022011096
+# فایلهای خود را در این پوشه قرار دهید
+git add submissions/4022011096/
+git commit -m "Add project 1 submission for 4022011096"
+git push origin submission/project1
