653af53d8cdcb49578341f377557557f8e27e06a
mahya yarmohammadi <mahyayarmohamadi@gmail.com>
2026-07-21 13:23:10 +0330

    Delete 4022011096 directory

--- 4022011096/project1_numeric.R
+++
@@ -1,5 +0,0 @@
-mkdir submissions/4022011096
-# فایلهای خود را در این پوشه قرار دهید
-git add submissions/4022011096/
-git commit -m "Add project 1 submission for 4022011096"
-git push origin submission/project1
